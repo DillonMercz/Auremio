219238634eb1b22e71e527aff9379c27b78f824e
Kneegrowtreegrow <wizardplays1111@gmail.com>
2024-11-23 21:31:30 -0500

    Setup GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,30 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Auremio',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-    domains: ['localhost'],
   },
-  experimental: {
-    optimizeCss: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/shop',
-        destination: '/shop/all',
-        permanent: true,
-      },
-    ]
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
